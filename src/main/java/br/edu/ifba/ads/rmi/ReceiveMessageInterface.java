6243fea88bd421b98b969f8f119c7a53f7a06687
rodrigo <rodrigocsilva63@gmail.com>
2022-05-23 22:36:22 -0300

    remove unnecessary itens to interface

--- src/main/java/br/edu/ifba/ads/rmi/ReceiveMessageInterface.java
+++ src/main/java/br/edu/ifba/ads/rmi/ReceiveMessageInterface.java
@@ -1,11 +1,9 @@
 package br.edu.ifba.ads.rmi;
 
 import java.rmi.*;
-import java.util.ArrayList;
+
 public interface ReceiveMessageInterface extends Remote
 {
-        public ArrayList buffer = new ArrayList();
-        public Semaphore mutex = new Semaphore(0);
         public void addItem() throws RemoteException;
         public void removeItem() throws RemoteException;
         public void addBuffer(int contador) throws RemoteException;        
